93728e57ee4e4166c85bb8ad9ef7b1042b29b471
DovahChief <pepelimonta@gmail.com>
2017-12-18 16:45:34 -0600

    Agregando solucion a problema de multiplos

--- src/main/java/com/sixdelta/Multiplos.java
+++ src/main/java/com/sixdelta/Multiplos.java
@@ -1,11 +1,22 @@
 package com.sixdelta;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by jose on 18/12/17.
  */
 public class Multiplos {
 
-    int calculateFor(int n){
-        return 23;
+    int calculateFor(int n) throws RuntimeException {
+        if(n <= 0){
+            RuntimeException e = new RuntimeException();
+            throw e;
+        }
+
+        int res = IntStream.range(1, n)
+                 .filter(i -> i%3 == 0 || i%5 == 0)
+                 .sum();
+
+        return res;
     }
 }
